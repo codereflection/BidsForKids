2e9777cd874c4e0c5dc8446bc7415285a0d194cd
JeffSchumacher <JeffSchumacher@acf1a558-4196-46e1-90b5-69a17c6da0ca>
2009-10-22 08:08:17 +0000

    Renamed file to match class.
    git-svn-id: http://svn.xp-dev.com/svn/BidForKids@14 acf1a558-4196-46e1-90b5-69a17c6da0ca

---
+++ src/trunk/BidForKids.Tests/Controllers/DonorControllerFacts.cs
@@ -0,0 +1,127 @@
+using System.Web.Mvc;
+using Xunit;
+using BidForKids.Controllers;
+using BidForKids.Models;
+using Rhino.Mocks;
+using System.Collections.Generic;
+using System;
+
+namespace BidForKids.Tests.Controllers
+{
+    public class DonorControllerFacts
+    {
+        public class Index : BidForKidsController
+        {
+            [Fact]
+            public void ReturnsViewResultWithDefaultViewName()
+            {
+                // Arrange
+                var controller = new DonorController(_ProcurementFactory);
+
+                // Act
+                var result = controller.Index();
+
+                // Assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Empty(viewResult.ViewName);
+            }
+
+            [Fact]
+            public void SetsViewDataWithContactModel()
+            {
+                // Arrange
+                var controller = new DonorController(_ProcurementFactory);
+
+                // Act
+                var result = controller.Index();
+
+                // Assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Empty(viewResult.ViewName);
+                Assert.IsType<List<Donor>>(viewResult.ViewData.Model);
+            }
+        }
+
+        public class Create : BidForKidsController
+        {
+            [Fact]
+            public void ReturnsViewResultWithDefaultViewName()
+            {
+                // Arrange
+                var controller = new DonorController(_ProcurementFactory);
+
+                // Act
+                var result = controller.Create();
+
+                // Assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<SelectList>(viewResult.ViewData["GeoLocation_ID"]);
+                Assert.Empty(viewResult.ViewName);
+            }
+        }
+
+        public class Edit : BidForKidsController
+        {
+            [Fact]
+            public void ReturnsViewResultWithDefaultViewName()
+            {
+                // Arrange
+                var controller = new DonorController(_ProcurementFactory);
+
+                // Act
+                var result = controller.Edit(0);
+
+                // Assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Empty(viewResult.ViewName);
+            }
+
+            [Fact]
+            public void SetsViewDataWithContactModel()
+            {
+                // Arrange
+                var controller = new DonorController(_ProcurementFactory);
+
+                // Act
+                var result = controller.Edit(0);
+
+                // Assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.IsType<SelectList>(viewResult.ViewData["GeoLocation_ID"]);
+                Assert.IsType<Donor>(viewResult.ViewData.Model);
+            }
+        }
+
+        public class Details : BidForKidsController
+        {
+            [Fact]
+            public void ReturnsViewResultWithDefaultViewName()
+            {
+                // Arrange
+                var controller = new DonorController(_ProcurementFactory);
+
+                // Act
+                var result = controller.Details(0);
+
+                // Assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Empty(viewResult.ViewName);
+            }
+
+            [Fact]
+            public void SetsViewDataWithContactModel()
+            {
+                // Arrange
+                var controller = new DonorController(_ProcurementFactory);
+
+                // Act
+                var result = controller.Details(0);
+
+                // Assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                Assert.Empty(viewResult.ViewName);
+                Assert.IsType<Donor>(viewResult.ViewData.Model);
+            }
+        }
+    }
+}
