6a5bad9c132db746af1e079f2ac2d6124952c952
Jeff Schumacher <jeff@codingreflection.com>
2010-09-15 06:43:45 -0700

    Adding BidsForKids.Data project for data layer abstraction

---
+++ src/BidsForKids.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34dfe3a1-557d-413a-b470-a6c705a3aae4")]
