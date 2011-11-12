0c561ec3b36772c8a24bf7ad6ebda690ab5d8849
codereflection <jeff@codingreflection.com>
2011-11-12 14:02:13 -0800

    Setting up more migrations, still not done though. Need to build scripts for the triggers and foreign keys

---
+++ src/BidsForKids.Database/Migration001.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using FluentMigrator;
+
+namespace BidsForKids.Database
+{
+    [Migration(001)]
+    public class Migration001 : Migration
+    {
+        public override void Up()
+        {
+            CreateAuctionTable();
+            CreateCategoryTable();
+            CreateContactProcurementTable();
+            CreateDonatesTable();
+            CreateDonorTable();
+            CreateDonorTypeTable();
+            CreateGeoLocationTable();
+            CreateLocationCodeMapTable();
+            CreateProcurementTable();
+            CreateProcurementHistoryTable();
+            CreateProcurementDonorTable();
+            CreateProcurementTypeTable();
+            CreateProcurerTable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        void CreateAuctionTable()
+        {
+            Create.Table("Auction")
+                .WithColumn("Auction_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("Year").AsInt16().NotNullable()
+                .WithColumn("Name").AsString(255).NotNullable();
+        }
+
+        void CreateCategoryTable()
+        {
+            Create.Table("Category")
+                .WithColumn("Category_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("CategoryName").AsString(255).NotNullable()
+                .WithColumn("Description").AsString(1000).Nullable();
+        }
+
+        void CreateContactProcurementTable()
+        {
+            Create.Table("ContactProcurement")
+                .WithColumn("ContactProcurement_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("Donor_ID").AsInt16().NotNullable()
+                .WithColumn("Procurement_ID").AsInt16().NotNullable()
+                .WithColumn("Auction_ID").AsInt16().NotNullable()
+                .WithColumn("Procurer_ID").AsInt16().Nullable();
+        }
+
+        void CreateDonatesTable()
+        {
+            Create.Table("Donates")
+                .WithColumn("Donates_ID").AsInt16().PrimaryKey().NotNullable()
+                .WithColumn("Description").AsString(20).NotNullable();
+        }
+
+        void CreateDonorTable()
+        {
+            Create.Table("Donor")
+                .WithColumn("Donor_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("BusinessName").AsString(255).Nullable()
+                .WithColumn("FirstName").AsString(255).Nullable()
+                .WithColumn("LastName").AsString(255).Nullable()
+                .WithColumn("Address").AsString(255).Nullable()
+                .WithColumn("City").AsString(255).Nullable()
+                .WithColumn("State").AsString(2).NotNullable()
+                .WithColumn("ZipCode").AsString(10).Nullable()
+                .WithColumn("Phone1").AsString(20).Nullable()
+                .WithColumn("Phone1Desc").AsString(10).Nullable()
+                .WithColumn("Phone2").AsString(20).Nullable()
+                .WithColumn("Phone2Desc").AsString(10).Nullable()
+                .WithColumn("Phone3").AsString(20).Nullable()
+                .WithColumn("Phone3Desc").AsString(10).Nullable()
+                .WithColumn("Notes").AsString(int.MaxValue).Nullable()
+                .WithColumn("Website").AsString(1024).Nullable()
+                .WithColumn("Email").AsString(255).Nullable()
+                .WithColumn("GeoLocation_ID").AsInt16().Nullable()
+                .WithColumn("Donates").AsBoolean().Nullable()
+                .WithColumn("CreatedOn").AsDateTime().Nullable()
+                .WithColumn("ModifiedOn").AsDateTime().Nullable()
+                .WithColumn("MailedPacket").AsBoolean().Nullable()
+                .WithColumn("Procurer_ID").AsInt16().Nullable()
+                .WithColumn("DonorType_ID").AsInt16().Nullable()
+                .WithColumn("Closed").AsBoolean().Nullable();
+
+            Execute.Sql("CREATE Trigger_Donor_Set_CreatedOn HERE");
+            Execute.Sql("CREATE Trigger_Donor_Set_ModifiedOn HERE");
+        }
+
+        void CreateDonorTypeTable()
+        {
+            Create.Table("DonorType")
+                .WithColumn("DonorType_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("DonorTypeDesc").AsString(50).NotNullable();
+        }
+
+        void CreateGeoLocationTable()
+        {
+            Create.Table("GeoLocation")
+                .WithColumn("GeoLocation_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("GeoLocationName").AsString(255).NotNullable()
+                .WithColumn("Description").AsString(1000).Nullable();
+        }
+
+        void CreateLocationCodeMapTable()
+        {
+            Create.Table("LocationCodeMap")
+                .WithColumn("LocationCode").AsString(255).Nullable()
+                .WithColumn("GeoLocation_ID").AsInt16().Nullable();
+        }
+
+        void CreateProcurementTable()
+        {
+            Create.Table("Procurement")
+                .WithColumn("Procurement_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("Description").AsString(2000).NotNullable()
+                .WithColumn("Quantity").AsFloat().Nullable()
+                .WithColumn("PerItemValue").AsDecimal(4, 10).Nullable()
+                .WithColumn("Notes").AsString(int.MaxValue).Nullable()
+                .WithColumn("CatalogNumber").AsString(20).Nullable()
+                .WithColumn("AuctionNumber").AsString(20).Nullable()
+                .WithColumn("ItemNumber").AsString(20).Nullable()
+                .WithColumn("EstimatedValue").AsDecimal(4, 10).Nullable()
+                .WithColumn("SoldFor").AsDecimal(4, 10).Nullable()
+                .WithColumn("Cateory_ID").AsInt16().Nullable()
+                .WithColumn("Origin_ID").AsInt16().Nullable()
+                .WithColumn("CreatedOn").AsDateTime().NotNullable()
+                .WithColumn("ModifiedOn").AsDateTime().Nullable()
+                .WithColumn("Donation").AsString(2000).Nullable()
+                .WithColumn("ThankYouLetterSent").AsBoolean().Nullable()
+                .WithColumn("Limitations").AsString(500).Nullable()
+                .WithColumn("Certificate").AsString(10).Nullable()
+                .WithColumn("ProcurementType_ID").AsInt16().Nullable()
+                .WithColumn("Title").AsString(2000).Nullable();
+
+            Execute.Sql("CREATE Trigger_Procurement_DELETE");
+            Execute.Sql("CREATE Trigger_Procurement_UPDATE");
+            Execute.Sql("CREATE Trigger_Set_CreatedOn");
+            Execute.Sql("CREATE Trigger_Set_ModifiedOn");
+        }
+
+        void CreateProcurementHistoryTable()
+        {
+            Create.Table("ProcurementHistory")
+                .WithColumn("ProcurementHistory_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("Operation").AsString(6).NotNullable()
+                .WithColumn("Procurement_ID").AsInt16().NotNullable()
+                .WithColumn("Description").AsString(2000).NotNullable()
+                .WithColumn("Quantity").AsFloat().Nullable()
+                .WithColumn("PerItemValue").AsDecimal(4, 10).Nullable()
+                .WithColumn("Notes").AsString(int.MaxValue).Nullable()
+                .WithColumn("CatalogNumber").AsString(20).Nullable()
+                .WithColumn("AuctionNumber").AsString(20).Nullable()
+                .WithColumn("ItemNumber").AsString(20).Nullable()
+                .WithColumn("EstimatedValue").AsDecimal(4, 10).Nullable()
+                .WithColumn("SoldFor").AsDecimal(4, 10).Nullable()
+                .WithColumn("Cateory_ID").AsInt16().Nullable()
+                .WithColumn("Origin_ID").AsInt16().Nullable()
+                .WithColumn("CreatedOn").AsDateTime().NotNullable()
+                .WithColumn("ModifiedOn").AsDateTime().Nullable()
+                .WithColumn("Donation").AsString(2000).Nullable()
+                .WithColumn("ThankYouLetterSent").AsBoolean().Nullable()
+                .WithColumn("Limitations").AsString(500).Nullable()
+                .WithColumn("Certificate").AsString(10).Nullable()
+                .WithColumn("ProcurementType_ID").AsInt16().Nullable()
+                .WithColumn("Title").AsString(2000).Nullable();
+        }
+
+        void CreateProcurementDonorTable()
+        {
+            Create.Table("ProcurementDonor")
+                .WithColumn("ProcurementDonor_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("Procurement_ID").AsInt16().NotNullable()
+                .WithColumn("Donor_ID").AsInt16().NotNullable();
+        }
+
+        void CreateProcurementTypeTable()
+        {
+            Create.Table("ProcurementType")
+                .WithColumn("ProcurementType_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("ProcurementTypeDesc").AsString(50).NotNullable()
+                .WithColumn("DonorType_ID").AsInt16().Nullable();
+        }
+
+        void CreateProcurerTable()
+        {
+            Create.Table("Procurer")
+                .WithColumn("Procurer_ID").AsInt16().PrimaryKey().Identity().NotNullable()
+                .WithColumn("FirstName").AsString(255).Nullable()
+                .WithColumn("LastName").AsString(255).Nullable()
+                .WithColumn("Phone").AsString(20).Nullable()
+                .WithColumn("Email").AsString(255).Nullable()
+                .WithColumn("Description").AsString(255).Nullable();
+        }
+    }
+}
