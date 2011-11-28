f391663613aa698c803a2741a1892e39d258ec7c
codereflection <jeff@codingreflection.com>
2011-11-27 16:33:15 -0800

    Added migrateLocal rake task (local Albacore needs to be updated)

--- rakefile.rb
+++ rakefile.rb
@@ -18,6 +18,16 @@ exec :solutionPackageRestore do |cmd|
     cmd.parameters = "install src/.nuget/packages.config -o src/packages"
 end
 
+task :migrateLocal => [:clean,:assemblyInfo,:build,:migrate_local]
+
+fluentmigrator :migrate_local do |fm|
+    fm.command = "src/packages/FluentMigrator.1.0.1.0/tools/Migrate.exe"
+    fm.connection = "Data Source=(local);Initial Catalog=BidForKids;Integrated Security=True"
+    fm.provider = "sqlserver2000"
+    fm.namespace = "BidsForKids.Database"
+    fm.target = "build/BidsForKids.Database.dll"
+end
+
 msbuild :build do |msb|
 	msb.properties :configuration => :AutomatedRelease
 	msb.solution = "src/BidsForKids.sln"
