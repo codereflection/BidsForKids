48b57559bb9c0d2079c6c651f47720872b3d172c
codereflection <jeff@codingreflection.com>
2012-01-22 16:45:54 -0800

    Upgrading ProjectVersion

--- src/ProjectVersion.cs
+++ src/ProjectVersion.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Gatewood Elementary PTA")]
 [assembly: AssemblyProduct("Bids For Kids Auction Management System")]
 [assembly: AssemblyCopyright("Copyright (c)2010-2011 Gatewood Elementary PTA")]
-[assembly: AssemblyVersion("1.2.1.5")]
-[assembly: AssemblyFileVersion("1.2.1.5")]
+[assembly: AssemblyVersion("1.2.1.6")]
+[assembly: AssemblyFileVersion("1.2.1.6")]
 
