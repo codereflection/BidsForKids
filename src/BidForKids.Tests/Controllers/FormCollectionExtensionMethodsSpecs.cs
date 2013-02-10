dfbe3ef6ec31e4cd991f95cf58bab0942e3c09ff
codereflection <jeff@codingreflection.com>
2013-02-10 01:47:58 -0800

    Fix to accept all DonorIds coming in numerically named

--- src/BidForKids.Tests/Controllers/FormCollectionExtensionMethodsSpecs.cs
+++ src/BidForKids.Tests/Controllers/FormCollectionExtensionMethodsSpecs.cs
@@ -20,7 +20,7 @@ namespace BidsForKids.Tests.Controllers
             result.Count.ShouldEqual(2);
     }
 
-    public class when_getting_a_list_of_values_from_numerically_named_ids_from_a_form_collection
+    public class when_getting_a_list_of_values_from_numerically_named_ids_from_a_form_collection_where_the_first_does_not_have_a_number
     {
         static FormCollection collection;
         static List<string> result;
@@ -33,5 +33,38 @@ namespace BidsForKids.Tests.Controllers
 
         It should_return_a_list_of_all_the_values = () =>
             result.Count.ShouldEqual(2);
+    }
+
+    public class when_getting_a_list_of_values_from_numerically_named_ids_from_a_form_collection_where_all_are_numbered
+    {
+        static FormCollection collection;
+        static List<string> result;
+
+        Establish context = () =>
+            collection = new FormCollection { { "DonorId_1", "1" }, { "DonorId_2", "2" } };
+
+        Because of = () =>
+            result = collection.GetDonorIdsFromFormCollection("DonorId");
+
+        It should_return_a_list_of_all_the_values = () =>
+            result.Count.ShouldEqual(2);
+    }
+
+    public class when_getting_a_list_of_values_where_the_id_is_not_in_the_collection
+    {
+        static FormCollection collection;
+        static List<string> result;
+
+        Establish context = () =>
+            collection = new FormCollection { { "Key", "Value" } };
+
+        Because of = () => 
+            result = collection.GetDonorIdsFromFormCollection("DonorId");
+
+        It should_not_return_null = () =>
+            result.ShouldNotBeNull();
+
+        It should_return_an_empty_collection = () =>
+            result.Count.ShouldEqual(0);
     }
 }
--- src/BidForKids/Controllers/FormCollectionExtensionMethods.cs
+++ src/BidForKids/Controllers/FormCollectionExtensionMethods.cs
@@ -8,8 +8,6 @@ namespace BidsForKids.Controllers
     {
         public static List<string> GetDonorIdsFromFormCollection(this FormCollection collection, string donorSelectFieldId)
         {
-            if (string.IsNullOrEmpty(collection[donorSelectFieldId])) return null;
-
             var items = collection.AllKeys.Where(item => item.StartsWith(donorSelectFieldId)).ToList();
 
             var donorIds = new List<string>();
